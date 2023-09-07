1aebb9ca95cade16175060833f4434dc4d0b7202
Kolja Bauer <kolja.bauer@gmail.com>
2023-09-07 18:06:40 +0200

    env kwargs in config file

--- sofa_zoo/envs/rope_threading/ppo.py
+++ sofa_zoo/envs/rope_threading/ppo.py
@@ -26,90 +26,39 @@ class PPOIterativeExperiment(experiment.AbstractIterativeExperiment):
         normalize_reward = True
         reward_clip = np.inf
 
-        # observations, bimanual, randomized, eyes
-        self.parameters = ["STATE", "False", "True", "1"]
-
-        observation_type = ObservationType[self.parameters[0]]
-        image_based = observation_type in [ObservationType.RGB, ObservationType.RGBD]
-
-        eye_configs = {
-            "1": [
-                (60, 10, 0, 90),
-            ],
-            "2": [
-                (60, 10, 0, 90),
-                (10, 10, 0, 90),
-            ],
-        }
-
-        bimanual_grasp = self.parameters[1] == "True"
-        randomized_eye = self.parameters[2] == "True"
-        image_based = observation_type in [ObservationType.RGB, ObservationType.RGBD]
 
         env_kwargs = {
             "image_shape": (64, 64),
             "window_size": (200, 200),
-            "observation_type": observation_type,
+            "observation_type": ObservationType["STATE"],
             "time_step": 0.01,
             "frame_skip": 10,
             "settle_steps": 20,
-            "render_mode": RenderMode.HEADLESS if image_based or add_render_callback else RenderMode.NONE,
+            "render_mode": RenderMode.NONE,
             #"render_mode": RenderMode.HUMAN,
-            "reward_amount_dict": {
-                "passed_eye": 10.0,
-                "lost_eye": -20.0,  # more than passed_eye
-                "goal_reached": 100.0,
-                "distance_to_active_eye": -0.0,
-                "lost_grasp": -0.1,
-                "collision": -0.1,
-                "floor_collision": -0.1,
-                "bimanual_grasp": 0.0,
-                "moved_towards_eye": 200.0,
-                "moved_away_from_eye": -200.0,
-                "workspace_violation": -0.01,
-                "state_limit_violation": -0.01,
-                "distance_to_lost_rope": -0.0,
-                "delta_distance_to_lost_rope": -0.0,
-                "fraction_rope_passed": 0.0,
-                "delta_fraction_rope_passed": 200.0,
-            },
-            "create_scene_kwargs": {
-                "eye_config": eye_configs[self.parameters[3]],
-                "randomize_gripper": False,
-                "start_grasped": True,
-                "randomize_grasp_index": False,
-            },
             "on_reset_callbacks": None,
             "color_eyes": True,
             "individual_agents": False,
-            "only_right_gripper": not bimanual_grasp,
+            "only_right_gripper": True,
             "fraction_of_rope_to_pass": 0.05,
-            "num_rope_tracking_points": 10,
         }
 
         create_scene_kwargs = config['params'].get('create_scene_kwargs', {})
-        env_kwargs['control_gripper_aperture'] = create_scene_kwargs.get('control_gripper_aperture', False)
+        env_kwargs['control_gripper_aperture'] = create_scene_kwargs.pop('control_gripper_aperture', False)
+        reward_amount_dict = config['params'].get('reward_amount_dict', {})
+        env_kwargs['create_scene_kwargs'] = create_scene_kwargs
+        env_kwargs['reward_amount_dict'] = reward_amount_dict
+
+        num_rope_tracking_points = config['params'].get('num_rope_tracking_points', 0)
+        env_kwargs['num_rope_tracking_points'] = num_rope_tracking_points
+
+
         env_kwargs['normalize_obs_static'] = config['params'].get('normalize_obs_static', False)
         normalize_obs_dynamic = config['params'].get('normalize_obs_dynamic', False)
 
-        if bimanual_grasp:
-            env_kwargs["reward_amount_dict"]["bimanual_grasp"] = 100.0
-            env_kwargs["reward_amount_dict"]["distance_to_bimanual_grasp"] = -0.0
-            env_kwargs["reward_amount_dict"]["delta_distance_to_bimanual_grasp"] = -200.0
-
-        if randomized_eye:
-            env_kwargs["create_scene_kwargs"]["eye_reset_noise"] = {
-                "low": np.array([-20.0, -20.0, 0.0, -15]),
-                "high": np.array([20.0, 20.0, 0.0, 15]),
-            }
-
-        # config = {"max_episode_steps": 200 + 150 * (len(eye_configs[self.parameters[3]]) - 1), **CONFIG}
         self.config = {"max_episode_steps": 100, **CONFIG}
 
-        if image_based:
-            ppo_kwargs = PPO_KWARGS["image_based"]
-        else:
-            ppo_kwargs = PPO_KWARGS["state_based"]
+        ppo_kwargs = PPO_KWARGS["state_based"]
 
         info_keywords = [
             "distance_to_active_eye",
@@ -184,7 +133,7 @@ class PPOIterativeExperiment(experiment.AbstractIterativeExperiment):
         self.model.learn(
             total_timesteps=self.config["total_timesteps"],
             callback=self.callback,
-            tb_log_name=f"{self.parameters[0]}_{self.parameters[1]}Biman_{self.parameters[2]}Random_{self.parameters[3]}",
+            tb_log_name=f"test_PPO",
         )
 
         return None
